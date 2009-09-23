9f7d4b3385a12cf972d8bf5f2f3ca875d8093ce9
troya <troya@scriptgeek.org>
2009-09-23 18:48:43 +0800

    search

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,19 @@
+require 'xmlrpc/client'		
+class SearchController < ApplicationController
+	def search
+		server = XMLRPC::Client.new( "localhost", "/services")
+
+		# Call the remote server and get our result
+		result = server.call("SearchResource.search", "wokao")
+		render :text => result.to_json
+		
+	end
+	
+	def upsertIndex
+		server = XMLRPC::Client.new( "localhost", "/services")
+
+		# Call the remote server and get our result
+		result = server.call("CreateUpdateResource.upsertResource", "title1")
+		render :text => result.to_json
+	end
+end
